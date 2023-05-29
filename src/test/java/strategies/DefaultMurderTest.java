d77208413ae17d84598898d4942bacfc7008682b
tuloksara <100419092+tuloksara@users.noreply.github.com>
2023-05-29 03:44:39 +0200

    Update DefaultMurderTest.java

--- src/test/java/strategies/DefaultMurderTest.java
+++ src/test/java/strategies/DefaultMurderTest.java
@@ -5,23 +5,29 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class DefaultMurderTest {
 
     @Test
     public void testMurder() {
-        main.java.virologist.Virologist murderer = new main.java.virologist.Virologist("John");
-        main.java.virologist.Virologist victim = new main.java.virologist.Virologist("Jane");
-        List<main.java.virologist.Virologist> virologists = main.java.gameControl.GameController.getInstance().getAllVirologists();
-        virologists.add(murderer);
-        virologists.add(victim);
+        main.java.virologist.Virologist murderer = mock(main.java.virologist.Virologist.class);
+        main.java.virologist.Virologist victim = mock(main.java.virologist.Virologist.class);
+
+        when(murderer.getName()).thenReturn("Attila");
+        when(victim.getName()).thenReturn("Kecske");
+
+        main.java.fields.Field f = new main.java.fields.Field();
+        // egy mezőn állnak
+        when(victim.getField()).thenReturn(f);
+        when(murderer.getField()).thenReturn(f);
 
         main.java.strategies.DefaultMurder strategy = new main.java.strategies.DefaultMurder();
 
         // Perform the murder
         strategy.murder(murderer, victim);
 
-        //az áldozat nem halt meg
-        assertTrue(virologists.contains(victim));
+        // az áldozat nem halt meg
+        verify(victim, never()).die();
     }
 }
